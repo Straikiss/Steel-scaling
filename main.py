3d453b5e397b5a9a6969c45c046bb28a7feb9d55
Straikiss <starko.web@gmail.com>
2017-03-10 17:40:00 -0800

    main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# damage at 15 lvl
+d_punch = 72 
+d_bull_rush = 210 
+d_shield_slam = 430 
+
+# scaling
+s_punch = 0.6
+s_bull_rush = 0.8
+s_shield_slam = 0.9
+
+#points
+card_point = 6 
+    
+cards = int(input("Enter cards: "))    
+cards *= card_point
+
+#add scaling * cards
+s_punch *= cards
+s_bull_rush *= cards
+s_shield_slam *= cards
+
+# add damage + scaling
+d_punch += s_punch
+d_bull_rush += s_bull_rush
+d_shield_slam += s_shield_slam
+
+print("[LMB] Punch:", d_punch)
+print("[Q] Bull rush:", d_bull_rush)
+print("[R] Shield slam:", d_shield_slam)
+
+
+
+
+
+
+
+
